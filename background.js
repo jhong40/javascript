a692974da92a03970c3fa34fc3115528ede08c22
jhong40 <jhong40@gmail.com>
2021-11-22 20:18:19 -0500

    Add files via upload

---
+++ background.js
@@ -0,0 +1,2 @@
+console.log("blah");
+console.log("aaa");
---
+++ f.js
@@ -0,0 +1,26 @@
+console.log("from f.js beging")
+
+var jq = document.createElement('script');
+jq.src = "https://code.responsivevoice.org/responsivevoice.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
+
+
+
+
+setInterval(myTimer, 5000);
+
+function myTimer() {
+
+  //price = document.querySelector('[data-reactid="47"]');
+  //price=price.textContent;
+  //price= parseFloat(price.split(',').join(''));
+  
+  //if (price>1156.00) {
+    var msg = new SpeechSynthesisUtterance("One hundred years later");
+    window.speechSynthesis.speak(msg);
+  //}
+  
+
+}
+
+console.log("from f.js end")
